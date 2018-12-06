4581469dd9f5f07951554d71564bbecc43326e3a
Lee1219 <anais3872@hanyang.ac.kr>
2018-12-06 15:02:17 +0900

    #7 issue 에러메시지 출력용

---
+++ UI/ErrorMessage.py
@@ -0,0 +1,50 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+'''
+최초작성자 : 이영찬
+최초작성일 : 2018.11.29
+최초변경일 :
+목적 : 에러 메시지 생성
+개정 이력 :
+'''
+class errorMsg(object):
+    def setupUi(self, Error):
+        Error.setObjectName("Error")
+        Error.resize(457, 138)
+        self.verticalLayout = QtWidgets.QVBoxLayout(Error)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.label = QtWidgets.QLabel(Error)
+        font = QtGui.QFont()
+        font.setFamily("나눔스퀘어라운드 ExtraBold")
+        font.setPointSize(12)
+        font.setBold(True)
+        font.setWeight(75)
+        self.label.setFont(font)
+        self.label.setAlignment(QtCore.Qt.AlignCenter)
+        self.label.setObjectName("label")
+        self.verticalLayout.addWidget(self.label)
+        self.pushButton = QtWidgets.QPushButton(Error)
+        self.pushButton.setObjectName("확인")
+        self.verticalLayout.addWidget(self.pushButton)
+
+        self.retranslateUi(Error)
+        self.pushButton.clicked.connect(Error.accept)
+        QtCore.QMetaObject.connectSlotsByName(Error)
+
+    def retranslateUi(self, Error):
+        _translate = QtCore.QCoreApplication.translate
+        Error.setWindowTitle(_translate("Error", "Warning!"))
+        self.label.setAccessibleName(_translate("Error", "errmsg"))
+        self.label.setText(_translate("Error", "에러 메시지가 출력되었습니다."))
+        self.pushButton.setAccessibleName(_translate("Error", "okay_button"))
+        self.pushButton.setText(_translate("Error", "확인"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Error = QtWidgets.QDialog()
+    ui = errorMsg()
+    ui.setupUi(Error)
+    Error.show()
+    sys.exit(app.exec_())
+
