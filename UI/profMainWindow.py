aa574b6b7a3bf06d1a90f9feac8be8a671bdbf4c
Lee1219 <anais3872@hanyang.ac.kr>
2018-12-07 15:14:37 +0900

    #7 issue 테이블 출력

--- UI/profMainWindow.py
+++ UI/profMainWindow.py
@@ -8,70 +8,64 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 개정이력:
 '''
 class view_ProfMainWindow(object):
+    def __init__(self, Info):
+        self.list = Info
+
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(452, 449)
+        MainWindow.resize(543, 590)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
-        self.horizontalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
-        self.horizontalLayoutWidget.setGeometry(QtCore.QRect(9, 9, 158, 25))
-        self.horizontalLayoutWidget.setObjectName("horizontalLayoutWidget")
-        self.horizontalLayout = QtWidgets.QHBoxLayout(self.horizontalLayoutWidget)
-        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
-        self.horizontalLayout.setObjectName("horizontalLayout")
-        self.pushButton = QtWidgets.QPushButton(self.horizontalLayoutWidget)
-        self.pushButton.setObjectName("추가")
-        self.horizontalLayout.addWidget(self.pushButton)
-        self.pushButton_2 = QtWidgets.QPushButton(self.horizontalLayoutWidget)
-        self.pushButton_2.setObjectName("설정")
-        self.horizontalLayout.addWidget(self.pushButton_2)
-        self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_3.setGeometry(QtCore.QRect(360, 10, 75, 23))
-        self.pushButton_3.setObjectName("ON/OFF")
-        self.pushButton_3.setCheckable(True)
+        self.gridLayout = QtWidgets.QGridLayout(self.centralwidget)
+        self.gridLayout.setObjectName("gridLayout")
         self.tableWidget = QtWidgets.QTableWidget(self.centralwidget)
-        self.tableWidget.setGeometry(QtCore.QRect(9, 40, 431, 381))
-        self.tableWidget.setObjectName("명단 리스트")
-        self.tableWidget.setColumnCount(6)
-        self.tableWidget.setRowCount(50)
+        self.tableWidget.setObjectName("tableWidget")
+        self.tableWidget.setColumnCount(4)
+        self.tableWidget.setRowCount(0)
         item = QtWidgets.QTableWidgetItem()
         self.tableWidget.setHorizontalHeaderItem(0, item)
-        item.setFlags(QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEnabled)
         item = QtWidgets.QTableWidgetItem()
         self.tableWidget.setHorizontalHeaderItem(1, item)
-        item.setFlags(QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEnabled)
         item = QtWidgets.QTableWidgetItem()
         self.tableWidget.setHorizontalHeaderItem(2, item)
-        item.setFlags(QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEnabled)
         item = QtWidgets.QTableWidgetItem()
         self.tableWidget.setHorizontalHeaderItem(3, item)
-        item.setFlags(QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEnabled)
-        item = QtWidgets.QTableWidgetItem()
-        self.tableWidget.setHorizontalHeaderItem(4, item)
-        item.setFlags(QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEnabled)
-        self.horizontalLayoutWidget_2 = QtWidgets.QWidget(self.centralwidget)
-        self.horizontalLayoutWidget_2.setGeometry(QtCore.QRect(410, 40, 151, 31))
-        self.horizontalLayoutWidget_2.setObjectName("horizontalLayoutWidget_2")
-        self.horizontalLayout_2 = QtWidgets.QHBoxLayout(self.horizontalLayoutWidget_2)
-        self.horizontalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.gridLayout.addWidget(self.tableWidget, 2, 0, 1, 2)
+        self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.gridLayout.addWidget(self.pushButton_3, 0, 1, 1, 1)
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setObjectName("pushButton")
+        self.horizontalLayout.addWidget(self.pushButton)
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.horizontalLayout.addWidget(self.pushButton_2)
+        self.gridLayout.addLayout(self.horizontalLayout, 0, 0, 1, 1)
+        self.horizontalLayout_2 = QtWidgets.QHBoxLayout()
         self.horizontalLayout_2.setObjectName("horizontalLayout_2")
-        self.pushButton_4 = QtWidgets.QPushButton(self.horizontalLayoutWidget_2)
-        self.pushButton_4.setObjectName("수정")
+        self.pushButton_4 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_4.setObjectName("pushButton_4")
         self.horizontalLayout_2.addWidget(self.pushButton_4)
-        self.pushButton_5 = QtWidgets.QPushButton(self.horizontalLayoutWidget_2)
-        self.pushButton_5.setObjectName("삭제")
+        self.pushButton_5 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_5.setObjectName("pushButton_5")
         self.horizontalLayout_2.addWidget(self.pushButton_5)
+        self.gridLayout.addLayout(self.horizontalLayout_2, 1, 1, 1, 1)
         self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(380, 10, 101, 21))
         self.label.setAlignment(QtCore.Qt.AlignCenter)
         self.label.setObjectName("label")
+        self.gridLayout.addWidget(self.label, 1, 0, 1, 1)
         MainWindow.setCentralWidget(self.centralwidget)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
+        self.tableWidget.setSelectionBehavior(QtWidgets.QTableView.SelectRows)
+        self.tableWidget.setSelectionMode(QtWidgets.QAbstractItemView.SingleSelection)
+        self.tableWidget.setEditTriggers(QtWidgets.QTableWidget.NoEditTriggers)
 
         self.setTableWidgetData()
-        id = self.tableWidget.itemClicked()
+        idx = self.tableWidget.curruntRow()
 
         self.retranslateUi(MainWindow)
         #추가 버튼 클릭 시 addButtonClicked 함수 호출
@@ -79,27 +73,29 @@ class view_ProfMainWindow(object):
         #설정 버튼 클릭 시 setButtonClicked 함수 호출
         self.pushButton_2.clicked.connect(self.setButtonClicked)
         #ON/OFF 버튼 클릭 시 switButtonClicked 함수 호출
-        self.pushButton_3.toggled.connect(self.switButtonClicked)
+        self.pushButton_3.clicked.connect(self.switButtonClicked)
         #수정 버튼 클릭 시 modButtonClicked 함수 호출
         self.pushButton_4.clicked.connect(self.modButtonClicked)
         #삭제 버튼 클릭 시 delButtonClicked 함수 호출
-        self.pushButton_5.clicked.connect(self.delButtonClicked(id))
+        self.pushButton_5.clicked.connect(self.delButtonClicked(idx))
         self.tableWidget.activated['QModelIndex'].connect(self.tableWidget.update)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     #목적 : 테이블의 내용을 채운다.
     def setTableWidgetData(self):
-        list = display.
-        column_idx = {'학번': 0, '이름': 1, '연락처':2, '팀번호':3}
-        for k, v in list.items():
-            col = column_idx[k]
-            for row, val in enumerate(v):
-                item = QtWidgets.QTableWidgetItem(val)
-                if col == 3:
-                    item.setTextAlignment(QtCore.Qt.AlignVcenter | QtCore.Qt.AlignRight)
+        list = self.list
+        for row in range(len(list)):
+            item = QtWidgets.QTableWidgetItem(list[row].studentNo)
+            self.tableWidget.setItem(row, 0, item)
+            item = QtWidgets.QTableWidgetItem(list[row].studentName)
+            self.tableWidget.setItem(row, 1, item)
+            item = QtWidgets.QTableWidgetItem(list[row].studentPhone)
+            self.tableWidget.setItem(row, 2, item)
+            item = QtWidgets.QTableWidgetItem(list[row].studentTeamNo)
+            self.tableWidget.setItem(row, 3, item)
+            self.tableWidget.setSortingEnabled(True)
+            item.setTextAlignment(QtCore.Qt.AlignVcenter | QtCore.Qt.AlignRight)
 
-                self.tableWidget.setItem(row, col, item)
-                self.tableWidget.setSortingEnabled(True)
 
         self.tableWidget.resizeColumnsToContents()
         self.tableWidget.resizeRowsToContents()
@@ -119,7 +115,7 @@ class view_ProfMainWindow(object):
     def switButtonClicked(self):
         Professor.swtichOper()
         state = display.switchOnOff()
-        self.label.setText(state)
+        self.label.setText("state : ", state)
 
     #목적 : 학생 정보를 수정하기 위한 인터페이스를 띄우도록 요청한다.
     def modButtonClicked(self):
@@ -127,18 +123,12 @@ class view_ProfMainWindow(object):
         window.show()
 
     #목적 : 학생 정보를 삭제하기 위한 인터페이스를 띄우도록 요청한다.
-    def delButtonClicked(self, id):
-        professor.deleteStud(id)
+    def delButtonClicked(self,idx):
+        professor.deleteStud(idx)
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "메인화면"))
-        self.pushButton.setAccessibleName(_translate("MainWindow", "add_Button"))
-        self.pushButton.setText(_translate("MainWindow", "추가"))
-        self.pushButton_2.setAccessibleName(_translate("MainWindow", "set_Button"))
-        self.pushButton_2.setText(_translate("MainWindow", "설정"))
-        self.pushButton_3.setAccessibleName(_translate("MainWindow", "switch_button"))
-        self.pushButton_3.setText(_translate("MainWindow", "ON/OFF"))
         self.tableWidget.setAccessibleName(_translate("MainWindow", "InfoList"))
         item = self.tableWidget.horizontalHeaderItem(0)
         item.setText(_translate("MainWindow", "학번"))
@@ -148,6 +138,12 @@ class view_ProfMainWindow(object):
         item.setText(_translate("MainWindow", "연락처"))
         item = self.tableWidget.horizontalHeaderItem(3)
         item.setText(_translate("MainWindow", "팀번호"))
+        self.pushButton_3.setAccessibleName(_translate("MainWindow", "switch_button"))
+        self.pushButton_3.setText(_translate("MainWindow", "ON/OFF"))
+        self.pushButton.setAccessibleName(_translate("MainWindow", "add_Button"))
+        self.pushButton.setText(_translate("MainWindow", "추가"))
+        self.pushButton_2.setAccessibleName(_translate("MainWindow", "set_Button"))
+        self.pushButton_2.setText(_translate("MainWindow", "설정"))
         self.pushButton_4.setAccessibleName(_translate("MainWindow", "ModButton"))
         self.pushButton_4.setText(_translate("MainWindow", "수정"))
         self.pushButton_5.setAccessibleName(_translate("MainWindow", "delButton"))
